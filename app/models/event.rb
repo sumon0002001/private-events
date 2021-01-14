9c9304662bb8145a18d721ba69aab5048a2accdd
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-14 17:23:29 +0600

    create model and controller for attendence

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User', foreign_key :creator_id
+    belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+    has_many :attendances
+    has_many :attendee, through: :attendances, source: :user
+
+    scope :upcoming, -> { where('date >= ?', DateTime.now) }
+    scope :past, -> { where('date < ? ', DateTime.now) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :events, foreign_key: :creator_id, class_name: 'Event'
     
+    scope :upcoming, -> { where('date >= ?', Date.today) }
+    scope :past, -> { where('date < ? ', Date.today) }
 end
