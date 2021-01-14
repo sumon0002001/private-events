458963e5adede9e4b3cd34a10f05c1a248702804
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-14 13:55:21 +0600

    added sign_in, show and create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
+require 'date'
+
 class UsersController < ApplicationController
+    include UsersHelper
+
+    def new
+        if session[:current_user]
+            redirect_to root_path
+        else
+            @user = User.new
+        end
+    end
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            current_user(@user.id)
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+    def sign_in
+      redirect_to root_path if session[:current_user]
+    end
+    def sign_in_new
+        @user = User.find_by(username: params[:username])
+        if @user && params[:password] == @user.password
+          current_user(@user.id)
+          redirect_to root_path
+        else
+          redirect_to sign_in_path
+        end
+    end
+    def show
+        if session[:current_user]
+            @user = User.find(params[:id])
+            current_user(@user.id)
+            @date = date_now
+        else
+            redirect_to sign_in_path
+        end
+    end
+
+
+
+
+
+
+
 end
