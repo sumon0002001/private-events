4d9c36e973674ad614b3a242c1fcfa9c62648ba9
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-14 13:23:55 +0600

    added new routes for sign_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    def index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: %i[new create show]
+  get '/sign_in', to: 'users#sign_in'
+  post '/sign_in', to: 'users#sign_in_new'
 end
