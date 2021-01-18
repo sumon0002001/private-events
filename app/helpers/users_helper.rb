7591ab0ad25ab920707312ff50a5c965e463cb95
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-18 12:04:59 +0600

    fixed linters error

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-    def current_user(id)
-        session[:current_user] = User.find(id)
-        session[:current_user]
-    end
-    def user_params
-        params.require(:user).permit(:username, :password, :email)
-    end
+  def current_user(id)
+    session[:current_user] = User.find(id)
+    session[:current_user]
+  end
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User', foreign_key: :creator_id
-    has_many :attendances
-    has_many :attendee, through: :attendances, source: :user
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+  has_many :attendances
+  has_many :attendee, through: :attendances, source: :user
 
-    scope :upcoming, -> { where('date >= ?', DateTime.now) }
-    scope :past, -> { where('date < ? ', DateTime.now) }
+  scope :upcoming, -> { where('date >= ?', DateTime.now) }
+  scope :past, -> { where('date < ? ', DateTime.now) }
 end
