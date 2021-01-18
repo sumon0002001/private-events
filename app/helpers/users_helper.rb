9d9a1e08b6ba0b41818173f0b41fa560841ddd9d
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-18 12:08:25 +0600

    fixed errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
     session[:current_user] = User.find(id)
     session[:current_user]
   end
+  
   def user_params
     params.require(:user).permit(:username, :password, :email)
   end
